d5b2c40d4e328315fb68653c61a9ed1453ccc249
fbarsotti <francescobarsotti02@gmail.com>
2023-10-17 21:06:46 +0200

    feat(general): removed unused import

--- lib/feature/songs/presentation/sections/song_card.dart
+++ lib/feature/songs/presentation/sections/song_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:risuscito/core/infrastructure/localization/app_localizations.dart';
